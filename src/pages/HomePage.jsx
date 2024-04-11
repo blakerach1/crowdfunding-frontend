9b77c7d9dc7eb63c70573c05073396483d89ddd9
Rachel Blake <blakerach1@gmail.com>
2024-04-11 22:01:08 +0800

    styling added for hero banner

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import ProjectCards from "../components/Projects/ProjectCards"; // import the ProjectCard component
 import useProjects from "../hooks/use-projects";
 import "../pages/HomePage.css"; // import the HomePage.css file
+import HeroBanner from "../components/Home/HeroBanner.jsx"
 
 function HomePage() {
   const { projects, isLoading, error } = useProjects(); // use the useProjects hook to get the projects
@@ -16,6 +17,7 @@ function HomePage() {
   return (
     <>
       <section className="home-page-content">
+        <HeroBanner />
         <div className="supporting-info">
           <p className="first-p">In today's world, investors seek more than just financial gains. 
             They crave a sense of purpose and connection.</p> 
